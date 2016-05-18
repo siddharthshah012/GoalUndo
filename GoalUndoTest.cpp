ab5f58eadd7cd544b350df46d9dcbe571bc314a0
siddharthshah012 <sshah8@mail.csuchico.edu>
2016-05-18 10:09:27 -0700

    First Commit of GoalUndo

--- GoalUndoTest.cpp
+++ GoalUndoTest.cpp
@@ -4,6 +4,8 @@
 
 #include <gtest/gtest.h>
 #include "GoalUndo.h"
+
+ using namespace std;
  
 class GoalUndoTest : public ::testing::Test
 {
@@ -17,4 +19,202 @@ class GoalUndoTest : public ::testing::Test
 TEST(GoalUndoTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(GoalUndoTest, undogoalemptyCheck)
+{
+	GoalUndo goalundo;
+	goalundo.undoGoal();
+	string tocheck ="";
+	ASSERT_EQ(tocheck, goalundo.getGoal());
+}
+
+TEST (GoalUndoTest, UndoGoalTestCheck)
+{
+	GoalUndo goalundo;
+	string tocheck = "Test Check";
+	string emptycheck = "";
+	goalundo.addOperation(tocheck);
+	goalundo.undoGoal();
+	ASSERT_EQ(emptycheck, goalundo.getGoal());
+}
+
+TEST (GoalUndoTest, emptyCheckUndoOperationsingle)
+{
+	GoalUndo goalundo;
+	goalundo.undoGoal();
+	string emptycheck = "";
+	ASSERT_EQ(emptycheck, goalundo.getGoal());
+}
+
+
+TEST (GoalUndoTest, singleoperationUndooperation)
+{
+	GoalUndo goalundo;
+	string tocheck = "Test 1";
+	string emptycheck = "";
+	goalundo.addOperation(tocheck);
+	goalundo.undoOperation();
+	ASSERT_EQ(emptycheck, goalundo.getGoal());
+}
+
+TEST(GoalUndoTest, multipleOperationUndooperation)
+{
+	GoalUndo goalundo;
+	string tocheck = "Test 1";
+	string tocheck1 = "Test 2";
+	goalundo.addOperation(tocheck);
+	goalundo.addOperation(tocheck1);
+	goalundo.undoOperation();
+	ASSERT_EQ(tocheck, goalundo.getGoal());
+}
+
+
+TEST (GoalUndoTest, emptyCheckUndoOperations)
+{
+	GoalUndo goalundo;
+	string emptyCheck = "";
+	string tocheck = " Test Check";
+	goalundo.undoOperation(tocheck);
+	ASSERT_EQ(emptyCheck, goalundo.getGoal());
+}
+
+
+TEST (GoalUndoTest, SingleOperationOneArgumentUndoOperation)
+{
+	GoalUndo goalundo;
+	string tocheck = "Test check";
+	string emptyCheck = "";
+	goalundo.addOperation(tocheck);
+	goalundo.undoOperation(tocheck);
+	ASSERT_EQ(emptyCheck, goalundo.getOperations());
+}
+
+TEST (GoalUndoTest, MultipleOperationOneArgumentSameUndoOperation)
+{
+	GoalUndo goalundo;
+	string tocheck = "Test check";
+	string tocheck1 = "Test check 1";
+	goalundo.addOperation(tocheck);
+	goalundo.addOperation(tocheck1);
+	goalundo.undoOperation(tocheck);
+	ASSERT_EQ(tocheck1, goalundo.getOperations());
+}
+
+TEST (GoalUndoTest, MultipleOperationOneArgumentSameUndoOperationCheck)
+{
+	GoalUndo goalundo;
+	string tocheck = "Test check";
+	string tocheck1 = "Test check 1";
+	string tocheck2 = "Random";
+	string tocheckresult = "Test check Test check 1";
+	goalundo.addOperation(tocheck);
+	goalundo.addOperation(tocheck1);
+	goalundo.undoOperation(tocheck2);
+	ASSERT_EQ(tocheckresult, goalundo.getOperations());
+}
+
+
+
+TEST (GoalUndoTest, emptyGetGoalCheck)
+{
+  GoalUndo goalundo;
+  string emptycheck = "";
+  ASSERT_EQ (emptycheck, goalundo.getGoal());
+}
+
+TEST (GoalUndoTest, GetGoalCheck)
+{
+	GoalUndo goalundo;
+	string tocheck = "";
+	ASSERT_EQ(tocheck,goalundo.getGoal());
+}
+
+TEST (GoalUndoTest, EmptyCheckGetOperations)
+{
+	GoalUndo goalundo;
+	string emptycheck = "";
+	ASSERT_EQ (emptycheck,goalundo.getOperations());
+}
+
+
+TEST (GoalUndoTest, CheckGetOperations)
+{
+	GoalUndo goalundo;
+	string tocheck = " TEST Check";
+	goalundo.addOperation(tocheck);
+	ASSERT_EQ(tocheck, goalundo.getOperations());
+}
+
+TEST (GoalUndoTest, MultipleOperationsUsingUndoOperations)
+{
+	GoalUndo goalundo;
+	string tocheck = " TEST Check";
+	string tocheck1 = "TEST Check 1";
+	string tocheck2 = tocheck + " " + tocheck1;
+	goalundo.addOperation(tocheck);
+	goalundo.addOperation(tocheck1);
+	ASSERT_EQ (tocheck2, goalundo.getOperations());
+}
+
+TEST (GoalUndoTest, EmptyTwoArgumentsCheckAddOperations)
+{
+	GoalUndo goalundo; 
+	string tocheck = "";
+	goalundo.addOperation(tocheck);
+	ASSERT_EQ(tocheck, goalundo.getGoal());
+}
+
+TEST (GoalUndoTest, TwoArguementsCheckAddOperations)
+{
+	GoalUndo goalundo;
+	string tocheck = "Test Check";
+	string emptycheck = "";
+	goalundo.addOperation(tocheck,emptycheck);
+	ASSERT_EQ(emptycheck,goalundo.getGoal());
+}
+
+TEST (GoalUndoTest,swapTwoArgumentsCheckAddOperations)
+{
+	GoalUndo goalundo;
+	string tocheck= "Test check";
+	string emptycheck = "";
+	goalundo.addOperation(emptycheck, tocheck);
+	ASSERT_EQ(emptycheck, goalundo.getGoal());
+}
+
+TEST (GoalUndoTest, TwoArguementsCheckAddOperationsNotEmpty)
+{
+	GoalUndo goalundo;
+	string tocheck= "Test check";
+	string tocheck1 = "Test Check 1";
+	goalundo.addOperation(tocheck, tocheck1 );
+	ASSERT_EQ (tocheck, goalundo.getGoal());
+}
+
+TEST (GoalUndoTest, OneArguementNotEmptyCheckAddOperations)
+{
+	GoalUndo goalundo;
+	string tocheck = "Test check";
+	goalundo.addOperation(tocheck);
+	ASSERT_EQ( tocheck, goalundo.getGoal());
+}
+
+TEST (GoalUndoTest, OneArguementNotEmptyCheckAddOperationsCheck)
+{
+	GoalUndo goalundo;
+	string tocheck = "Test check";
+	string tocheck1 = "Test check 1";
+	goalundo.addOperation(tocheck);
+	goalundo.addOperation(tocheck1);
+	ASSERT_EQ (tocheck, goalundo.getGoal() );
+	//ASSERT_EQ( tocheck, goalundo.getGoal());
+}
+
+TEST (GoalUndoTest, OneArguementEmptyCheckAddOperations)
+{
+	GoalUndo goalundo;
+	string tocheck = "";
+	goalundo.addOperation(tocheck);
+	ASSERT_EQ (tocheck, goalundo.getGoal());
 }
